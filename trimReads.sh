5ce44a50a965790d813b8b34869777a400d47695
Andy Madrid <76069657+andymadrid@users.noreply.github.com>
2023-02-07 18:03:53 -0600

    Update trimReads.sh

--- trimReads.sh
+++ trimReads.sh
@@ -6,8 +6,8 @@
   --path_to_cutadapt ~/.local/bin/cutadapt 
   -q 30 
   --length 30 
-  --clip_R1 50 
-  --clip_R2 50
-  --three_prime_clip_R1 50 
-  --three_prime_clip_R2 50 
+  --clip_R1 11 
+  --clip_R2 11
+  --three_prime_clip_R1 5 
+  --three_prime_clip_R2 5 
   --paired reads_R1.fastq reads_R2.fastq
